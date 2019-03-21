e46d7fa78e9e4f6f0a335e4a7f2ee30b578f4542
Guvernant <i.daniil2010@yandex.ru>
2019-03-21 21:50:07 +0400

    cars test client

--- client.js
+++ client.js
@@ -1,13 +1,56 @@
 #!/usr/bin/env node
 var WebSocketClient = require('websocket').client
 
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout
+});
+
+rl.on("line", (input)=>{
+    var words = input.split(" ")
+    if (words[0] == "delivery"){
+        let model = words[1]
+        let color = words[2]
+        let year = words[3]
+        let price = words[4]
+
+        if (_connection != null){
+            requestDelivery(_connection,model,color,year,price)
+        }
+    }
+    else
+    if (words[0] == "purchase"){
+        let carid = words[1]
+        let fio = words[2]
+        let passport = words[3]
+        let method = words[4]
+
+        if (_connection != null){
+            requestPurchase(_connection,carid,fio,passport,method)
+        }
+    }
+    else
+    if (words[0] == "select"){
+        let array = words[1]
+
+        if (_connection != null){
+            requestSelect(_connection,array)
+        }
+    }
+})
+
+
 var client = new WebSocketClient()
+var _connection = null
 
 client.on('connectFailed', function(error) {
     console.log('Connect Error: ' + error.toString())
 })
 
 client.on('connect', function(connection) {
+    _connection = connection
 
     console.log('WebSocket Client Connected')
     connection.on('error', function(error) {
@@ -15,6 +58,7 @@ client.on('connect', function(connection) {
     })
     connection.on('close', function() {
         console.log('echo-protocol Connection Closed')
+        _connection = null
     })
     connection.on('message', function(message) {
         if (message.type === 'utf8') {
@@ -29,7 +73,57 @@ client.on('connect', function(connection) {
             setTimeout(sendNumber, 1000);
         }
     }
-    sendNumber();
+    //sendNumber();
 })
 
-client.connect('ws://95.104.236.120:8080/', 'echo-protocol')
+function requestDelivery(connection,model,color,year,price){
+    
+    var car = { 
+        model : model,
+        color : color,
+        year : year,
+        price : price
+    }
+
+    var request = {
+        type : "delivery",
+        car : car
+    }
+    
+
+    var query = JSON.stringify(request)
+    console.debug("Sent: " + query)
+    connection.sendUTF(query)
+}
+
+function requestPurchase(connection,carID,fio,passport,paymentMethod){
+
+    var request = {
+        type : "purchase",
+        carID : carID,
+        person : {
+            FIO : fio,
+            passport : passport,
+            paymentMethod : paymentMethod
+        },
+        date : Date()
+    }
+
+    var query = JSON.stringify(request)
+    console.debug("Sent: " + query)
+    connection.sendUTF(query)
+}
+
+function requestSelect(connection,array){
+
+    var request = {
+        type : "select",
+        array : array
+    }
+
+    var query = JSON.stringify(request)
+    console.debug("Sent: " + query)
+    connection.sendUTF(query)
+}
+
+client.connect('ws://95.104.217.68:8080/', 'echo-protocol')
